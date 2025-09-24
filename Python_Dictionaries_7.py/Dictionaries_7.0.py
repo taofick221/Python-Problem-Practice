76adef516af88b7d148175fe9cfcb16a808041a8
Md Taofick <mdtaofick45418@gmail.com>
2025-09-24 20:32:59 +0600

    Python dictionary code

---
+++ Python_Dictionaries_7.py/Dictionaries_7.0.py
@@ -0,0 +1,21 @@
+# Dictionarys in Python
+thisdict={
+    "brand":"ford",
+    "model":"mustang",  
+    "year":1964 
+}
+print(thisdict)
+
+# Print the "brand" value of the dictionary
+thisdict={
+    "brand":"ford", 
+    "model":"mustang",  
+    "year":1964
+}
+print(thisdict["brand"])
+
+# Using the dict() method to make a dictionary
+thisdict=dict(brand="ford", model="mustang", year=1964)
+print(thisdict)
+
+
