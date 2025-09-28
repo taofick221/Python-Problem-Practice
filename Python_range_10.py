2b748c180bd1a79912286ea3961c918926d7ad2e
Md Taofick <mdtaofick45418@gmail.com>
2025-09-28 20:40:48 +0600

    Python range code

---
+++ Python_range_10.py
@@ -0,0 +1,18 @@
+# Python range 
+for x in range(2, 30, 3):  
+    print(x)
+print()
+
+
+
+# Using List to Display Ranges
+print(list(range(5)))
+print(list(range(1, 6)))
+print(list(range(5, 20, 3)))
+
+
+
+
+
+
+
