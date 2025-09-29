fca13224495e669ecb7a76a1b82ec82f02a3fc71
Md Taofick <mdtaofick45418@gmail.com>
2025-09-29 20:47:16 +0600

    Python array code

---
+++ Python_Arrays_11.py
@@ -0,0 +1,14 @@
+# Create an array
+import array as arr
+
+# Create an array of integers
+integer_array = arr.array('i', [1, 2, 3, 4, 5]) 
+print("Integer Array:", integer_array)
+
+
+
+# Create an array of floats
+float_array = arr.array('f', [1.1, 2.2, 3.3, 4.4, 5.5])
+print("Float Array:", float_array)
+
+
