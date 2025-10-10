55fca3f568f05a8e82482d01bc914ecfa80dc2e8
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-10-10 23:55:49 +0600

    Update Python_Classes_and_Objects_13.1.py

--- Python_OOP_13.py/Python_Classes_and_Objects_13.1.py
+++ Python_OOP_13.py/Python_Classes_and_Objects_13.1.py
@@ -29,10 +29,6 @@ print(person2.greet())
 
 
 
-# Modify object properties
-person1.age = 31
-print(person1.greet())
-
 # Create another object of the Person class
 person2 = Person("Bob", 25)
 print(person2.greet())
