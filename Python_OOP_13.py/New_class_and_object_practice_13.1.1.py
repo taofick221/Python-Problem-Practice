a46b50e3ac1ee8aed55044ed2849f3591a67a43f
Md Taofick <mdtaofick45418@gmail.com>
2025-10-11 19:24:06 +0600

    updated python new class and object code

--- Python_OOP_13.py/New_class_and_object_practice_13.1.1.py
+++ Python_OOP_13.py/New_class_and_object_practice_13.1.1.py
@@ -14,9 +14,49 @@ class student:
           self.name =name
           self.age=age
           self.result=result
+    
+    def show(self):
+         print(f"Name: {self.name}, Age: {self.age}, Result:{self.result}")
+
 
 p1=student("Taofick",23,"A+")
 p2=student("Nahid",24,"A")
 
 print(p1.name)
 print(p2.age)
+p1.show()
+p2.show()
+# without str method
+print(p1)
+
+
+
+
+# The __str__() Method
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+      return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
+
+
+
+# Create Methods
+class student:
+   def __init__(self,name,age):
+     self.name=name
+     self.age=age
+    
+   def myfunction(self):
+    print("My name is " + self.name)
+
+
+p1 = student("Taofick",36)
+
+p1.myfunction()
