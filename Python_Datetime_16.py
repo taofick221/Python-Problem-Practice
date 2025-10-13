64e9e020e185cf8c711f9bb06255cf7815e1fc75
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-10-13 19:37:15 +0600

    Python datetime code

---
+++ Python_Datetime_16.py
@@ -0,0 +1,21 @@
+# Python Date
+import datetime
+
+x = datetime.datetime.now()
+
+print(x)
+
+
+
+# Return the year and name of weekday
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+
+
+
