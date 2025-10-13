6851905ed927caeef8d224167ec768d862a7e38f
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-10-13 19:56:13 +0600

    python input code

---
+++ Python_User_Input_17.py
@@ -0,0 +1,17 @@
+# User Input
+print("Enter your name")
+name=input()
+print(f"Hello {name}")
+
+
+
+
+# Multiple inputs
+name = input("Enter your name:")
+print(f"Hello {name}")
+fav1 = input("What is your favorite animal:")
+fav2 = input("What is your favorite color:")
+fav3 = input("What is your favorite number:")
+print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
+
+
