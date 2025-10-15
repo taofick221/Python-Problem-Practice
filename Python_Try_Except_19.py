ce189ec492cee2332d25f829b0c2490283a7459a
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-10-15 23:10:44 +0600

    python try and except with list and dictionary code

--- Python_Try_Except_19.py
+++ Python_Try_Except_19.py
@@ -23,3 +23,38 @@ try:
 except:
     print("Something went wrong when opening the file")
 
+# Raise an error and stop the program if x is lower than 0
+
+x = -1
+
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
+
+
+# Raise a TypeError if x is not an integer
+
+x = "hello"
+
+if not type(x) is int:
+  raise TypeError("Only integers are allowed")
+
+
+
+
+# list and a dictionary with try and except
+try:
+    fruits = ["apple", "banana", "cherry"]
+    prices = {"apple": 100, "banana": 60, "cherry": 120}
+
+    print(fruits[5])  
+
+    print(prices["mango"])  
+
+except IndexError:
+    print("List index is out of range!")
+
+except KeyError:
+    print("Key not found in dictionary!")
+
+finally:
+    print("Program finished.")
